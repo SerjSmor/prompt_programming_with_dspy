07f6183bc672caf20ff9ab4287d350071fa0d6b5
Brett-Kennedy <84815610+Brett-Kennedy@users.noreply.github.com>
2025-09-07 14:08:50 -0400

    Create Listing_4.4.py

---
+++ chapter_4/Listing_4.4.py
@@ -0,0 +1,25 @@
+import dspy
+import pandas as pd
+from datasets import load_dataset
+
+def create_examples_from_set(set_name, n=-1):
+   ds = load_dataset("tuetschek/atis")
+   ds.set_format(type='pandas')
+   df: pd.DataFrame = ds[set_name][:]
+   df['intent'] = df['intent'].map(ATIS_INTENT_MAPPING)
+   df = df.dropna(subset='intent')
+   unique_intents = df['intent'].unique()
+   if n > 0:
+      df = df.sample(n=n, random_state=42) 
+
+   examples = []
+   for index in df.index: 
+       row = df.loc[index]
+       examples.append(
+           dspy.Example(message=row['text'],
+                        labels=unique_intents,
+		       intent_label=row['intent']).with_inputs('message', 'labels')
+       )
+   return examples
+
+test_set = create_examples_from_set('test', 100)
