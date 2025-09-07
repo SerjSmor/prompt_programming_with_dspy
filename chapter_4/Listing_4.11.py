2a70cefd07bc2d6951ff26d2214494e1cfba8b68
Brett-Kennedy <84815610+Brett-Kennedy@users.noreply.github.com>
2025-09-07 14:11:39 -0400

    Create Listing_4.11.py

---
+++ chapter_4/Listing_4.11.py
@@ -0,0 +1,6 @@
+booking_flight_example = dspy.Example(
+    message="I want to book a flight", 
+    labels=unique_intents, 
+    intent_label='Flight Booking Request').with_inputs("message", "labels")
+booking_flight_prediction = Prediction(intent_label=’Airport Distance Inquiry’)
+print(validate_answer(booking_flight_example, booking_flight_prediction))
