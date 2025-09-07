c8d948341213913122987d341e946c3b31b5ed94
Brett-Kennedy <84815610+Brett-Kennedy@users.noreply.github.com>
2025-09-07 14:05:24 -0400

    Create Listing_3.10.py

---
+++ chapter_3/Listing_3.10.py
@@ -0,0 +1,16 @@
+class MultiIntentSignature(dspy.Signature):
+   """
+   Classify the message into one or more of the possible intent labels.
+   """
+   message: str      = dspy.InputField()
+   labels: List[str] = dspy.InputField()
+   intent_label: str = dspy.OutputField(desc="Return the closest matches if any are "
+      "reasonably close. Return as many as are close. Otherwise return None")
+
+message = "I'd like to book a flight to Madrid from Berlin and order the pasta dinner."
+labels = list(unique_intents)
+lm = dspy.LM("openai/gpt-4o-mini")
+dspy.settings.configure(lm=lm)
+classifier = dspy.Predict(MultiIntentSignature)
+prediction = classifier(message=message, labels=labels)
+print(prediction)
