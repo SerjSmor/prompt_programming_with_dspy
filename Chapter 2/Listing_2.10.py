30a0b733edb4be9d9b801db1a18cf5a8e6277666
Brett-Kennedy <84815610+Brett-Kennedy@users.noreply.github.com>
2025-09-07 13:59:30 -0400

    Create Listing_2.10.py

---
+++ Chapter 2/Listing_2.10.py
@@ -0,0 +1,12 @@
+dspy.configure_cache( 
+
+    enable_disk_cache=False,
+    enable_memory_cache=False,
+    enable_litellm_cache=False
+)
+lm = dspy.LM('openai/gpt-4o', cache=True, temperature=0.99) 
+dspy.configure(lm=lm)
+prog = dspy.Predict("question -> answer")
+print(prog(question="Compose a 5-line poem about space")) 
+print(prog(question="Compose a 5-line poem about space")) 
+print(len(lm.history))
