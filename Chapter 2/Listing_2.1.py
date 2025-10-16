3b2ad8d299fb0a9d1324ca5d37bbd0c8b1acfede
Serj Smor <serjsmor@gmail.com>
2025-10-16 09:02:56 +0300

    Add examples to optimization

--- Chapter 2/Listing_2.1.py
+++ Chapter 2/Listing_2.1.py
@@ -13,10 +13,20 @@ labels = ['Cancel Subscription', 'Update Email', 'Refund Request',
           'Bug Report', 'Unknown']
 examples = [dspy.Example({'message': 'I want to end my subscription',
                           'labels': labels,
-                          'intent_label': 'CancelSubscription'})
-            .with_inputs('message')]
+                          'intent_label': 'Cancel Subscription'},
+                         )
+            .with_inputs('message'),
+            dspy.Example({'message': 'I deserve a refund',
+                          'labels': labels,
+                          'intent_label': 'Refund Request'}
+                         ),
+            dspy.Example({'message': 'The login screen is frozen, I cant login',
+                          'labels': labels,
+                          'intent_label': 'Bug Report'}
+                         )
+            ]
 classifier = dspy.ChainOfThought('message, labels -> intent_label')
-optimizer = dspy.BootstrapFewShot(metric=msg_to_intent_metric, max_labeled_demos=3)
+optimizer = dspy.BootstrapFewShot(metric=msg_to_intent_metric, max_labeled_demos=2)
 classifier = optimizer.compile(classifier, trainset=examples)
 prediction = classifier(
     message="My new email address is bob.smith@domain.com", labels=labels)
