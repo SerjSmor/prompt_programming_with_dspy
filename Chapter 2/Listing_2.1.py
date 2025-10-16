54bc20b082de90ad5d813131452dfe85aa9e6618
Serj Smor <serjsmor@gmail.com>
2025-10-16 08:52:04 +0300

    Code formatting

--- Chapter 2/Listing_2.1.py
+++ Chapter 2/Listing_2.1.py
@@ -1,18 +1,22 @@
 import dspy
+import os
 
-def msgToIntentMetric(example, pred, trace=None):
+
+def msg_to_intent_metric(example, pred, trace=None):
     return pred.intent_label == example.intent_label
 
+
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 lm = dspy.LM("openai/gpt-4o-mini", api_key=OPENAI_API_KEY)
 dspy.settings.configure(lm=lm)
-labels = ['Cancel Subscription', 'Update Email', 'Refund Request', 
+labels = ['Cancel Subscription', 'Update Email', 'Refund Request',
           'Bug Report', 'Unknown']
-examples = [dspy.Example({'message': 'I want to end my subscription', 
+examples = [dspy.Example({'message': 'I want to end my subscription',
                           'labels': labels,
-                          'intent_label': 'CancelSubscription'})\
-                    .with_inputs('message')] 
+                          'intent_label': 'CancelSubscription'})
+            .with_inputs('message')]
 classifier = dspy.ChainOfThought('message, labels -> intent_label')
-optimizer  = dspy.BootstrapFewShot(metric=msgToIntentMetric, max_labeled_demos=3)
+optimizer = dspy.BootstrapFewShot(metric=msg_to_intent_metric, max_labeled_demos=3)
 classifier = optimizer.compile(classifier, trainset=examples)
 prediction = classifier(
     message="My new email address is bob.smith@domain.com", labels=labels)
