ca0cce160dd6ad9add417c263bcf37da35e76791
Serj Smor <serjsmor@gmail.com>
2025-10-16 09:19:57 +0300

    Code formatting

--- Chapter 2/Listing_2.11.py
+++ Chapter 2/Listing_2.11.py
@@ -1,19 +1,19 @@
 import os
 import dspy
 
-os.environ['OPENAI_API_KEY'] = OPENAI_API_KEY
+OPENAI_API_KEY = os.environ['OPENAI_API_KEY']
 
-lm_mini  = dspy.LM(model='gpt-4o-mini', cache=False, temperature=0.0)
+lm_mini = dspy.LM(model='gpt-4o-mini', cache=False, temperature=0.0)
 lm_turbo = dspy.LM(model='gpt-3.5-turbo', cache=False, temperature=0.0)
 dspy.settings.configure(lm=lm_turbo)
 prog = dspy.Predict('question->answer')
-prediction = prog(question='Write a 5-line poem about toasters') 
+prediction = prog(question='Write a 5-line poem about toasters')
 print(prediction)
 
 with dspy.context(lm=lm_mini):
-  prediction = prog(question='Write a 5-line poem about toasters') 
-  print(prediction)
+    prediction = prog(question='Write a 5-line poem about toasters')
+    print(prediction)
 
 prog.set_lm(lm_turbo)
-prediction = prog(question='Write a 5-line poem about toasters') 
+prediction = prog(question='Write a 5-line poem about toasters')
 print(prediction)
